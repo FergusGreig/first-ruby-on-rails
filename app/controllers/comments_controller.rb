9da89c0a32ea0e46ec692552d6dcfbf7f84ce0e8
FergusGreig <fgreig@outlook.com>
2020-10-19 12:39:55 +0100

    easily hackable authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
+
+    http_basic_authenticate_with name:"ferg"
+    password: "notarealpassword"
+    only: :destroy
+
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
